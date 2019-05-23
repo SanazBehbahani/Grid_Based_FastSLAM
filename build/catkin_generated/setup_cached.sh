3a1557af5afb0377103cdba113d581968efbc9da
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-23 20:27:54 +0000

    add slam_gmapping package

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/workspace/FastSLAM/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/workspace/FastSLAM/build"
