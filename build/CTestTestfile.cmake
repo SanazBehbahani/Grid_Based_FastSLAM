10ce6ab503a4c5aee3320d8986bb2e25e00ba2b4
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-23 20:19:46 +0000

    initialize workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/FastSLAM/src
+# Build directory: /home/workspace/FastSLAM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
